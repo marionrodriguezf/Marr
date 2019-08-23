0170a04375bafc5e2d22cc4bbf8055615c16cac7
marionrodriguezf <54457058+marionrodriguezf@users.noreply.github.com>
2019-08-23 17:55:12 -0500

    Update Calculadora.java

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -99,10 +99,8 @@ public int getResultado() {
               
     }
         
-    
-    
-
         
+          
  
     switch(opcion){
         case 4:
@@ -117,4 +115,93 @@ public int getResultado() {
     }
     }
     
+}
+
+package calculadora;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Estudiantes
+ */
+public class CalculadoraOra {
+        int Dato1,Dato2,datosi;
+        double Resultado;
+        
+
+void sumar(){
+    
+   Resultado=Dato1+Dato2;
+}
+
+void restar(){
+    
+  Resultado= Dato1-Dato2;
+    
+}
+
+void multiplicar(){
+    
+  Resultado= Dato1*Dato2;
+           
+}
+
+void Dividir(){
+    
+    if(Dato2==0){
+       System.out.println("¡error!¡No puedes dividir sobre 0!");
+    }
+    else{
+        Resultado= Dato1/Dato2;
+    }
+     
+}
+
+void setDato1 (int a){
+ 
+    Dato1=a;
+}
+
+void setDato2(int b){
+    
+    Dato2=b;
+}
+
+public double getResultado() {
+        return Resultado;
+    }
+}
+
+package calculadora;
+import static java.lang.Math.*;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class CalculadoraTrigonometrica extends CalculadoraOra{
+    
+    
+     void seno(boolean grado){
+       double radian =(Dato1*PI)/180;
+       Resultado=sin(radian);
+    }
+     
+    void seno(){
+        Resultado=sin(Dato1);
+    }
+    
+    void coseno(){
+        Resultado=cos(Dato1);
+    }
+   void coseno(boolean grado){
+       double radian =(Dato1*PI)/180;
+       Resultado=cos(radian);
+    }
+    void tangente(){
+        Resultado=tan(Dato1);
+    }
+    void tangente(boolean grado){
+       double radian =(Dato1*PI)/180;
+       Resultado=tan(radian);
+    }
 }
